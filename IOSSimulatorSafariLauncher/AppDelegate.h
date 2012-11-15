52f545aa516d02dbe9c231282e162deaac0b3ec3
Michael Schieben <babe@rockitbaby.de>
2012-11-15 15:33:32 +0100

    Initial Commit

---
+++ IOSSimulatorSafariLauncher/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IOSSimulatorSafariLauncher
+//
+//  Created by Michael Schieben on 15.11.12.
+//  Copyright (c) 2012 Michael Schieben. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
